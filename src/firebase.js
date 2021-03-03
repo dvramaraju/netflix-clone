8000169282c7361f791807c5eda747ac461c883e
dvramaraju <ramarajudv21@gmail.com>
2021-03-03 15:50:31 +0530

    Clean Up Commit

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCFyQybhJTmF5usASbghznaIP5Qt6KM26w",
+  authDomain: "netflix-db5bf.firebaseapp.com",
+  projectId: "netflix-db5bf",
+  storageBucket: "netflix-db5bf.appspot.com",
+  messagingSenderId: "444762653327",
+  appId: "1:444762653327:web:162ad93e91ba5fc08966fa",
+  measurementId: "G-DXVD81N4CF",
+};
